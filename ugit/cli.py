4267d2d1af4c1c64c48dab08e1cfec7d0f1eda68
guye1296 <ezer.guy@gmail.com>
2020-10-13 01:06:32 +0300

    add hash_object subcommand

--- ugit/cli.py
+++ ugit/cli.py
@@ -12,12 +12,21 @@ def parse_args() -> argparse.Namespace:
     init_parser = commands.add_parser('init')
     init_parser.set_defaults(func=init)
 
+    hash_object_parser = commands.add_parser('hash_object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument('file')
+
     return parser.parse_args()
 
 
 def init(args):
     print(f"initialized emtpty ugit repository in {os.path.join(os.getcwd(), data.GIT_DIR)}")
     return data.init()
+
+
+def hash_object(args):
+    with open(args.file, 'rb') as file:
+        print(data.hash_object(file.read()))
 
 
 def main():
--- ugit/data.py
+++ ugit/data.py
@@ -1,7 +1,17 @@
 import os
+import hashlib
 
 GIT_DIR = '.ugit'
+OBJECTS_DIR = os.path.join(GIT_DIR, 'objects')
 
 
 def init():
-    return os.makedirs(GIT_DIR)
+    os.makedirs(GIT_DIR)
+    os.makedirs(OBJECTS_DIR)
+
+
+def hash_object(data) -> str:
+    oid = hashlib.sha1(data).hexdigest()
+    with open(os.path.join(OBJECTS_DIR, oid), 'wb') as out:
+        out.write(data)
+    return oid
