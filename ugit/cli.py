10bfea7cb80fece0d3e1578d6a01acf81478ea69
guye1296 <ezer.guy@gmail.com>
2020-10-13 00:39:37 +0300

    add init subcommand parser

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,2 +1,22 @@
+import argparse
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    commands = parser.add_subparsers(dest='command')
+    commands.required = True
+
+    init_parser = commands.add_parser('init')
+    init_parser.set_defaults(func=init)
+
+    return parser.parse_args()
+
+
+def init():
+    raise NotImplemented
+
+
 def main():
-    print('hello world!')
+    args = parse_args()
+
